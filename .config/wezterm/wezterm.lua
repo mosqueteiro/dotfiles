fb7682b70cb438b37ee6bee613d2aee47ca2d8d2
Mosqueteiro <nat3.th3.gr3at@gmail.com>
2025-12-17 22:52:24 -0700

    [wezterm] Add wezterm config files

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,98 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+local act = wezterm.action
+
+local workspace_selector = require("workspace_selector")
+
+-- TODO: Learn and/or adapt copy+pasta and scroll back with keyboard
+-- TODO: Check if unix_domains even needed here.
+-- TODO: Setup a whichkey type of functionality
+-- TODO: Can InputSelector be inverted?
+-- TODO: Investigate if there is a way to have a smaller window for Input selector (like command palette)
+-- TODO: Add wezterm setup to dotfiles
+--
+-- Skip:
+-- TODO: Make a few pre-configured workspaces (e.g. dotfiles, neovim config, home)
+
+--------------------------------------------------------------------------------
+-- Visuals settings
+--------------------------------------------------------------------------------
+config.window_decorations = "RESIZE"
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
+config.colors = {
+	cursor_bg = "white",
+	-- cursor_border = "white",
+}
+config.font = wezterm.font("DaddyTimeMono Nerd Font")
+
+--- Display workspace name
+wezterm.on("update-status", function(window, pane)
+	local cws = wezterm.mux.get_active_workspace()
+	-- wezterm.log_info("current workspace: " .. cws)
+	window:set_left_status(wezterm.format({
+		{ Attribute = { Intensity = "Bold" } },
+		{ Text = "[" .. cws .. "]" },
+	}))
+end)
+
+--------------------------------------------------------------------------------
+-- Keybindings
+--------------------------------------------------------------------------------
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 2000 }
+
+config.keys = {
+	{ key = "-", mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ key = "|", mods = "LEADER|SHIFT", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	-- Navigate Panes
+	{ key = "h", mods = "ALT", action = act.ActivatePaneDirection("Left") },
+	{ key = "j", mods = "ALT", action = act.ActivatePaneDirection("Down") },
+	{ key = "k", mods = "ALT", action = act.ActivatePaneDirection("Up") },
+	{ key = "l", mods = "ALT", action = act.ActivatePaneDirection("Right") },
+	-- Resize Panes
+	{ key = "H", mods = "ALT", action = act.AdjustPaneSize({ "Left", 5 }) },
+	{ key = "J", mods = "ALT", action = act.AdjustPaneSize({ "Down", 5 }) },
+	{ key = "K", mods = "ALT", action = act.AdjustPaneSize({ "Up", 5 }) },
+	{ key = "L", mods = "ALT", action = act.AdjustPaneSize({ "Right", 5 }) },
+	{ key = "LeftArrow", mods = "ALT", action = act.AdjustPaneSize({ "Left", 1 }) },
+	{ key = "LeftArrow", mods = "ALT|SHIFT", action = act.AdjustPaneSize({ "Left", 5 }) },
+	{ key = "DownArrow", mods = "ALT", action = act.AdjustPaneSize({ "Down", 1 }) },
+	{ key = "DownArrow", mods = "ALT|SHIFT", action = act.AdjustPaneSize({ "Down", 5 }) },
+	{ key = "UpArrow", mods = "ALT", action = act.AdjustPaneSize({ "Up", 1 }) },
+	{ key = "UpArrow", mods = "ALT|SHIFT", action = act.AdjustPaneSize({ "Up", 5 }) },
+	{ key = "RightArrow", mods = "ALT", action = act.AdjustPaneSize({ "Right", 1 }) },
+	{ key = "RightArrow", mods = "ALT|SHIFT", action = act.AdjustPaneSize({ "Right", 5 }) },
+
+	-- Tabs
+	{ key = "n", mods = "LEADER|CTRL", action = act.ActivateTabRelative(1) },
+	{ key = "p", mods = "LEADER|CTRL", action = act.ActivateTabRelative(-1) },
+	{ key = "c", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
+	{ key = "x", mods = "LEADER", action = act.CloseCurrentPane({ confirm = false }) },
+	{ key = "X", mods = "LEADER", action = act.CloseCurrentTab({ confirm = false }) },
+	{ key = "C", mods = "LEADER", action = act.SpawnWindow },
+
+	-- Launcher
+	{
+		key = "S",
+		mods = "LEADER",
+		action = act.ShowLauncherArgs({
+			flags = "FUZZY|WORKSPACES",
+			title = "launcher",
+		}),
+	},
+	{
+		key = "K",
+		mods = "LEADER",
+		action = wezterm.action_callback(workspace_selector.workspace_selector_callback),
+	},
+}
+
+config.unix_domains = {
+	{
+		name = "unix",
+	},
+}
+config.default_gui_startup_args = { "connect", "unix" }
+
+return config
---
+++ .config/wezterm/workspace_selector.lua
@@ -0,0 +1,73 @@
+--------------------------------------------------------------------------------
+-- Workspace
+--------------------------------------------------------------------------------
+local wezterm = require("wezterm")
+local act = wezterm.action
+
+local module = {}
+
+-- TODO: store last workspace and assign `LEADER L` to switch to last workspace
+
+--- Workspace selector callback function. Use with as an action.
+-- @param window The current window
+-- @param pane The current pane
+function module.workspace_selector_callback(window, pane)
+	local choices = {}
+
+	-- Add active workspaces
+	for _, ws in ipairs(wezterm.mux.get_workspace_names()) do
+		table.insert(choices, { label = " " .. ws, id = "workspace:" .. ws })
+	end
+
+	-- Add zoxide paths
+	local success, stdout, stderr =
+		wezterm.run_child_process({ "/home/mosqueteiro/.pixi/bin/zoxide", "query", "--list" })
+	if success then
+		for path in stdout:gmatch("[^\r\n]+") do
+			local label = path:gsub(wezterm.home_dir, "~")
+			table.insert(choices, { label = " " .. label, id = "path:" .. path })
+		end
+	elseif stderr then
+		wezterm.log_error("zoxide error: " .. stderr)
+	end
+
+	window:perform_action(
+		act.InputSelector({
+			action = wezterm.action_callback(function(inner_window, inner_pane, id, label)
+				if not id and not label then
+					wezterm.log_info("cancelled")
+				else
+					wezterm.log_info("id = " .. id)
+					wezterm.log_info("label = " .. label)
+
+					local workspace_type, identifier = id:match("([^:]+):(.+)")
+					local workspace_name
+
+					if workspace_type == "workspace" then
+						workspace_name = identifier
+					elseif workspace_type == "path" then
+						workspace_name = identifier:match("([^/]+)$")
+					else
+						wezterm.log_error("Unknown ID format: " .. id)
+						return
+					end
+
+					inner_window:perform_action(
+						act.SwitchToWorkspace({
+							name = workspace_name,
+							spawn = { cwd = identifier },
+						}),
+						inner_pane
+					)
+				end
+			end),
+			title = "Select Workspace",
+			choices = choices,
+			fuzzy = true,
+			fuzzy_description = " ",
+		}),
+		pane
+	)
+end
+
+return module
